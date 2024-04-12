d2c1d674e38ba6a35a9f0f6a40d84f7323ec4b21
Brian Ho <bh1525@nyu.edu>
2024-04-12 15:10:35 -0400

    audio

---
+++ bonus_audio001/.history/main_20240314201821.js
@@ -0,0 +1,5 @@
+import './style.css'
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
---
+++ bonus_audio001/.history/main_20240412144851.js
@@ -0,0 +1,109 @@
+import './style.css'
+import * as THREE from 'three'
+import { addBoilerPlateMesh, addStandardMesh } from './addMeshes'
+import { addLight } from './addLights'
+import Model from './Model'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	100
+)
+camera.position.set(0, 0, 5)
+
+//Globals
+const meshes = {}
+const lights = {}
+const mixers = []
+const clock = new THREE.Clock()
+const controls = new OrbitControls(camera, renderer.domElement)
+
+//Raycast
+const raycaster = new THREE.Raycaster()
+const pointer = new THREE.Vector2()
+
+//Audio
+const startTime = 0
+let endTime = 5
+init()
+function init() {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	document.body.appendChild(renderer.domElement)
+
+	//meshes
+	meshes.default = addBoilerPlateMesh()
+	meshes.standard = addStandardMesh()
+
+	//lights
+	lights.defaultLight = addLight()
+
+	//changes
+	meshes.default.scale.set(2, 2, 2)
+
+	//scene operations
+	scene.add(meshes.default)
+	scene.add(meshes.standard)
+	scene.add(lights.defaultLight)
+	raycast()
+	resize()
+	animate()
+}
+
+function raycast() {
+	window.addEventListener('click', (event) => {
+		pointer.x = (event.clientX / window.innerWidth) * 2 - 1
+		pointer.y = -(event.clientY / window.innerHeight) * 2 + 1
+		raycaster.setFromCamera(pointer, camera)
+		const intersects = raycaster.intersectObjects(scene.children, true)
+		for (let i = 0; i < intersects.length; i++) {
+			const object = intersects[0].object
+			console.log(object)
+			if (object.userData.groupName === 'target1') {
+				endTime += 5
+			}
+		}
+	})
+}
+
+function resize() {
+	window.addEventListener('resize', () => {
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+	})
+}
+
+function animate() {
+	requestAnimationFrame(animate)
+	const delta = clock.getDelta()
+
+	meshes.default.rotation.x += 0.01
+	meshes.default.rotation.z += 0.01
+
+	meshes.standard.rotation.x += 0.01
+	meshes.standard.rotation.z += 0.01
+
+	// meshes.default.scale.x += 0.01
+
+	renderer.render(scene, camera)
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	const audio = document.getElementById('audio')
+
+	audio.addEventListener('loadedmetadata', () => {
+		audio.currentTime = startTime // set start time
+	})
+
+	audio.addEventListener('timeupdate', () => {
+		if (audio.currentTime >= endTime) {
+			audio.pause() // pause the playback
+		}
+	})
+
+	audio.play() // start playing
+})
